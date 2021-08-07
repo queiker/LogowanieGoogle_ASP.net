d2b08faa2503b8f72967c619ca424a49782c44bd
Grzegorz Płonka Kraków ul.Źródlana 11 30-734 <tradecomp.pl@gmail.com>
2021-08-07 14:04:24 +0200

    first commit

---
+++ MyInventory/Controllers/AccountController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyInventory_base.Models;
+using MyInventory_base.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyInventory_base.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserRepository userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return View(new LoginModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = userRepository.GetByUsernameAndPassword(model.Username, model.Password);
+            
+            if (user == null)
+                return Unauthorized();
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier , user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("FavoriteColor", user.FavoritColor)
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                principal,
+                new AuthenticationProperties { IsPersistent = model.RememberLogin }
+                );
+
+            return LocalRedirect(model.ReturnUrl);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+
+        [AllowAnonymous]
+        public IActionResult LoginWithGoogle(string returnUrl = "/")
+        {
+            var props = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("GoogleLoginCallback"),
+                Items =
+                {
+                    {"returnUrl", returnUrl }
+                }
+            };
+
+            return Challenge(props, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> GoogleLoginCallback()
+        {
+            var result = await HttpContext.AuthenticateAsync
+                (CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var externalClaims = result.Principal.Claims.ToList();
+
+            var subjectIdCLaim = externalClaims.FirstOrDefault(
+                x => x.Type == ClaimTypes.NameIdentifier);
+
+            var subjectValue = subjectIdCLaim.Value;
+
+            var user = userRepository.GetByGoogleId(subjectValue);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("FavoriteColor", user.FavoritColor)
+            };
+
+            var identity = new ClaimsIdentity(claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principle = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme, principle);
+
+            return LocalRedirect(result.Properties.Items["returnUrl"]);
+        }
+
+    }
+}
